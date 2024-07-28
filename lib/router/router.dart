67b00431a222eb59fb1025800e222fad1658e0fd
egor <egorsborsikov250575@gmail.com>
2024-07-28 14:53:29 +0700

    refactor

---
+++ lib/router/router.dart
@@ -0,0 +1,8 @@
+import 'package:crypto_coins_list/features/crypto_coin/view/crypto_coin_screen.dart';
+import 'package:crypto_coins_list/features/crypto_list/view/crypto_list_screen.dart';
+
+final routes = {
+    '/': (context) => const CryptoListScreen(title: 'Crypto Currencies App'),
+    '/coin': (context) => const CryptoCoinScreen(),
+      
+};
---
+++ lib/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  scaffoldBackgroundColor: Colors.black,
+  primarySwatch: Colors.yellow,
+  dividerColor: Colors.white24,
+  appBarTheme: const AppBarTheme(
+  backgroundColor: Colors.black,
+  titleTextStyle: TextStyle(
+    color: Colors.white, 
+    fontSize: 20, 
+    fontWeight: FontWeight.w700
+    )
+  ),
+  listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+  textTheme: TextTheme(
+    bodyMedium: const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+      fontSize: 20,
+      ),
+    labelSmall: TextStyle(
+      color: Colors.white.withOpacity(0.6),
+      fontWeight: FontWeight.w700,
+      fontSize: 14,
+      )
+    ),
+  );
